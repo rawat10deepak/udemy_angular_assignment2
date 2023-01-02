b5ee3d1756a5246feba2d766b556fc32824611cf
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-02 17:23:13 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  userName = '';
+
+  resetUserName(){
+    this.userName = ''
+  }
+
+  disbleButton(){
+    return  this.userName === ''
+  }
 }
